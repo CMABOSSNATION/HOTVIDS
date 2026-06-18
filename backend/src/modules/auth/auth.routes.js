082aa92ddac319b7f32635f0158b7a4da793bd23
CMABOSSNATION <cmaraphael90@gmail.com>
2026-06-18 09:45:53 +0300

    Remove refresh route from auth.routes.js

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
-const { register, login, refresh } = require('./auth.controller');
+const { register, login } = require('./auth.controller');
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/refresh', refresh);
 
 module.exports = router;
